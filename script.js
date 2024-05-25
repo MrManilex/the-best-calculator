b4ce6d6873b3e15b6e6329fea9152ab675de7005
manilex <alexandermaniwang99@gmail.com>
2024-05-25 11:12:37 -0400

    refactor

--- script.js
+++ script.js
@@ -1,47 +1,53 @@
-const numberOne = document.getElementById('num-one')
-const numberTwo = document.getElementById('num-two')
-const numberThree = document.getElementById('num-three')
-const numberFour = document.getElementById('num-four')
-const numberFive = document.getElementById('num-five')
-const numberSix = document.getElementById('num-six')
-const numberSeven = document.getElementById('num-seven')
-const numberEight = document.getElementById('num-eight')
-const numberNine = document.getElementById('num-nine')
-const numberZero = document.getElementById('num-zero')
+const numberOneEl = document.getElementById('num-one')
+const numberTwoEl = document.getElementById('num-two')
+const numberThreeEl = document.getElementById('num-three')
+const numberFourEl = document.getElementById('num-four')
+const numberFiveEl = document.getElementById('num-five')
+const numberSixEl = document.getElementById('num-six')
+const numberSevenEl = document.getElementById('num-seven')
+const numberEightEl = document.getElementById('num-eight')
+const numberNineEl = document.getElementById('num-nine')
+const numberZeroEl = document.getElementById('num-zero')
+
+const subtractEl= document.getElementById('subtract')
+const addEl = document.getElementById('add')
 
 // retrieve the number displayer by ID
-const numberDisplayer = document.getElementById('num-displayer')
+const numDisplayEl = document.getElementById('num-displayer')
 
-const clear = document.getElementById('clear')
+const clearEl = document.getElementById('clear')
 
 // add event listeners to when the user clicks on the number div
-numberOne.addEventListener('click', display)
-numberTwo.addEventListener('click', display)
-numberThree.addEventListener('click', display)
-numberFour.addEventListener('click', display)
-numberFive.addEventListener('click', display)
-numberSix.addEventListener('click', display)
-numberSeven.addEventListener('click', display)
-numberEight.addEventListener('click', display)
-numberNine.addEventListener('click', display)
-numberZero.addEventListener('click', display)
+numberOneEl.addEventListener('click', display)
+numberTwoEl.addEventListener('click', display)
+numberThreeEl.addEventListener('click', display)
+numberFourEl.addEventListener('click', display)
+numberFiveEl.addEventListener('click', display)
+numberSixEl.addEventListener('click', display)
+numberSevenEl.addEventListener('click', display)
+numberEightEl.addEventListener('click', display)
+numberNineEl.addEventListener('click', display)
+numberZeroEl.addEventListener('click', display)
 
-clear.addEventListener('click', clearDisplay)
+subtractEl.addEventListener('click', display)
+addEl.addEventListener('click', display)
+
+clearEl.addEventListener('click', clearDisplay)
 
 function display() {
-    if (numberDisplayer.innerText == 0) {
+    if (numDisplayEl.innerText == 0) {
         // this asigns the text inside the number displayer as the initial number pressed/clicked on
-        numberDisplayer.innerText = this.innerText
-    } else if (numberDisplayer.innerText !== 0) {
-        // If the second number is pressed.... Append the number pressed.
-        numberDisplayer.innerText += this.innerText
+        numDisplayEl.innerText = this.innerText
+    } else if (this.innerText === '+' || this.innerText === '-') {
+        console.log('PRESSED')
     } else {
-        console.log('error')
+        // If the second number is pressed.... Append the number pressed.
+        numDisplayEl.innerText += this.innerText
     }
 
 }
 
 function clearDisplay() {
     // this function will be responsible for clearing all entries and memory
-    numberDisplayer.innerText = 0
+    numDisplayEl.innerText = 0
 }
